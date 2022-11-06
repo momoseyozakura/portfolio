81522a8559b30838ccb4443152575d448833488b
竹本廉 <takemotoren@takemotorennoMacBook-Air.local>
2022-11-06 23:42:56 +0900

    ユーザープロフィールページ実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get "/" => "home#index"
+  get "users/:id/show" => "users#show"
   get "users/:id/edit" => "users#edit"
   post "users/:id/update" => "users#update"
 end

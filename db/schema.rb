09340b20a11b6d385059d395001745c2084345e8
竹本廉 <takemotoren@takemotorennoMacBook-Air.local>
2022-12-28 00:52:17 +0900

    ユーザーにopenカラム追加、当選確認画面に行けるユーザーを制限

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_26_082137) do
+ActiveRecord::Schema.define(version: 2022_12_27_153950) do
 
   create_table "messages", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2022_12_26_082137) do
     t.string "image"
     t.text "member"
     t.boolean "admin", default: false
+    t.boolean "open", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

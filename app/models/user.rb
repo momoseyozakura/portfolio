d114dbe076a9ddb535fc700db90131d9b187e404
竹本廉 <takemotoren@takemotorennoMacBook-Air.local>
2022-12-29 01:31:42 +0900

    新規登録、ログイン時のエラーメッセージ表示実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
+
+  validates :email, :name, :password, :member, presence: true
 
   has_many :room_users, dependent: :destroy
   has_many :talk_rooms, through: :room_users

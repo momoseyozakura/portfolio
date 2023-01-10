6083e5c802a890c6f60c9a34db251c2e0dddc8b0
竹本廉 <takemotoren@takemotorennoMacBook-Air.local>
2023-01-10 16:37:14 +0900

    dm一覧のメッセージ表示を簡略化

--- app/helpers/talk_rooms_helper.rb
+++ app/helpers/talk_rooms_helper.rb
@@ -1,18 +1,8 @@
 module TalkRoomsHelper
-  # 最新メッセージのデータを取得して表示するメソッド
   def most_new_message_preview(room)
-    # 最新メッセージのデータを取得する
     message = room.messages.order(updated_at: :desc).limit(1)
-    # 配列から取り出す
     message = message[0]
-    # メッセージの有無を判定
-    if message.present?
-      # メッセージがあれば内容を表示
-      tag.p "#{message.body}"
-    else
-      # メッセージがなければ[ まだメッセージはありません ]を表示
-      tag.p "[ まだメッセージはありません ]"
-    end
+    tag.p "#{message.body}"
   end
 
   def opponent_user(room)

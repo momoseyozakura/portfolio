58156a7a2e6a38c716b8016a3791ae9e1ff4a261
竹本廉 <takemotoren@takemotorennoMacBook-Air.local>
2023-03-13 15:50:41 +0900

    dbをmysqlに変更

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2023_02_07_091725) do
+
   create_table "messages", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "talk_room_id", null: false
